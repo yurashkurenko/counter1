68857b6c5284a674095cd07ad58e56206a6e1e33
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-06-22 15:57:13 +0300

    Create settings.py

---
+++ mpy/settings.py
@@ -0,0 +1,5 @@
+deviceid="1212121212"
+url='https://tgdj.ubnt.cloudns.cl/devpara'
+TOKEN='1701291551:AAEk2TbH7mbHLPVVBrP0gEOSZBezbjH1FHY'
+CHATID=159085018
+URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'

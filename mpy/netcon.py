54bfa9aef5979e9063b694c46a723c48173ed7ff
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-06-22 15:05:57 +0300

    Create netcon.py

---
+++ mpy/netcon.py
@@ -0,0 +1,7 @@
+import network
+def connect(wifi,pwd):
+    sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+    print(sta_if.scan())                             # Scan for available access points
+    sta_if.connect(wifi,pwd) # Connect to an AP
+    # sta_if.connect("OnePlus 8 Pro", "n6ggk5gs") # Connect to an AP
+    print(sta_if.ifconfig())
